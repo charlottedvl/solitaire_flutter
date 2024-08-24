002b25d85e5881fd97cc1db56c93673091b6329f
charlottedvl <charlotte.deville@eleve.isep.fr>
2024-08-24 19:14:04 +0200

    Update victory screen

--- lib/screens/home_screen/home.dart
+++ lib/screens/home_screen/home.dart
@@ -3,73 +3,49 @@ import 'package:provider/provider.dart';
 import 'package:solitaire/backend/models/board.dart';
 import 'package:solitaire/backend/providers/boardProvider.dart';
 import 'package:solitaire/screens/home_screen/widget/game_button.dart';
-import 'package:solitaire/screens/home_screen/widget/nav_bar.dart';
-import 'package:solitaire/screens/home_screen/widget/title.dart';
-import 'package:solitaire/shared/constants.dart';
+import 'package:solitaire/screens/shared/screen_shape/title_button_screen.dart';
+import 'package:solitaire/shared/string_constants.dart';
+import 'package:solitaire/screens/shared/widget/custom_title.dart';
 
 class Home extends StatelessWidget {
   Home({
     super.key,
   });
 
-  double padding = 0.1;
-  double widthSizedBox = 0.1;
-  double heightSizedBox = 0.1;
-  double heightSpacing = 0.1;
   Board board = Board(false, null, null, null, null, null, null);
   int selectedIndex = 0;
+  List<String> cards = [
+    "Ready",
+    "To",
+    "Have",
+    "Fun",
+  ];
 
-  void calculateSize(double screenWidth, double screenHeight) {
-    padding = screenWidth * 0.1;
-    widthSizedBox = screenWidth * 0.02;
-    heightSizedBox = screenHeight * 0.05;
-    heightSpacing = screenHeight * 0.03;
+  List<Widget> getWidgets() {
+    return [
+      CustomTitle(WELCOME_MSG_EN, cards),
+      Column(
+        children: [
+          GameButton(
+            title: "Start new game",
+            isNewGameButton: true,
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          GameButton(
+            title: "Continue game",
+            isNewGameButton: false,
+          ),
+        ],
+      )
+    ];
   }
 
   @override
   Widget build(BuildContext context) {
     context.read<BoardProvider>().loadGame();
-    double screenWidth = MediaQuery.of(context).size.width;
-    double screenHeight = MediaQuery.of(context).size.height;
-    calculateSize(screenWidth, screenHeight);
-
-    return Scaffold(
-      backgroundColor: lightGreen,
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/images/media/background.jpg"),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: Padding(
-          padding: EdgeInsets.all(padding),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              MyTitle(widthSizedBox, heightSizedBox),
-              Column(
-                children: [
-                  GameButton(
-                    title: "Start new game",
-                    isNewGameButton: true,
-                  ),
-                  SizedBox(
-                    height: heightSpacing,
-                  ),
-                  GameButton(
-                    title: "Continue game",
-                    isNewGameButton: false,
-                  ),
-                ],
-              )
-            ],
-          ),
-        ),
-      ),
-      bottomNavigationBar: NavBar(
-        selectedIndex: selectedIndex,
-      ),
-    );
+    List<Widget> widgets = getWidgets();
+    return TitleButtonScreen(widgets: widgets, selectedIndex: selectedIndex);
   }
 }
