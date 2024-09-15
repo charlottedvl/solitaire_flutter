883776b412463a9c8f7791c5f8a81fa447f8325b
charlottedvl <charlotte.deville@eleve.isep.fr>
2024-09-15 16:32:21 +0200

    Format and add snack bar hint

--- lib/shared/utils.dart
+++ lib/shared/utils.dart
@@ -1,9 +1,7 @@
-
 class CustomUtils {
   String formatTime(int totalSeconds) {
     final minutes = totalSeconds ~/ 60;
     final seconds = totalSeconds % 60;
-    return '${minutes.toString().padLeft(2, '0')}:${seconds.toString().padLeft(
-        2, '0')}';
+    return '${minutes.toString().padLeft(2, '0')}:${seconds.toString().padLeft(2, '0')}';
   }
 }
